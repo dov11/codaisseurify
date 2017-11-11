a0b05468653eaf7248e4f892a07476d3ae2b8998
dov11 <ovo.dan@gmail.com>
2017-11-12 00:50:10 +0100

    add adding pictures to artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -14,6 +14,9 @@ class ArtistsController < ApplicationController
   def create
     @artist = Artist.new(artist_params)
     if @artist.save
+      pictures_params.each do |picture|
+        @artist.pictures.create(image_url: picture)
+      end
       redirect_to new_artist_song_path(@artist), notice: "Artist created, you can now add songs"
     else
       render :new
@@ -37,4 +40,9 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name, :active_since)
   end
+
+  def pictures_params
+    params[:pictures].present? ? params.require(:pictures) : []  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs
   end
+  resources :pictures, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
