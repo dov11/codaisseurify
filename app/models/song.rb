061ff9829d49f1ba3e56a20e7651b853bc5a9d64
dov11 <ovo.dan@gmail.com>
2017-11-11 14:12:18 +0100

    add song model validations and tests for them

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :length, numericality: {less_than_or_equal_to: 36000}
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -10,5 +10,17 @@ RSpec.describe Song, type: :model do
       expect(song.artist).to eq(artist)
     end
   end
-
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it "is invalid with a name longer than 100 characters" do
+      song = Song.new(name: Faker::Lorem.characters(101))
+      song.valid?
+      expect(song.errors).to have_key(:name)
+    end
+    it "is invalid if the song is longer than 10 hours" do
+      song = Song.new(length: 36001)
+      song.valid?
+      expect(song.errors).to have_key(:length)
+    end
+  end
 end
