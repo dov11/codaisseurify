1820539f344ce71df057719239e76b9e38504d8d
dov11 <ovo.dan@gmail.com>
2017-11-11 18:50:33 +0100

    add image resizing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,10 @@ include Cloudinary::CarrierWave
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  version :thumbnail do
+      eager
+      resize_to_fit(150, 150)
+      cloudinary_transformation :quality => 80
+    end
 
 end
