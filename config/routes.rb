0092946a1205a6506fd946771ff5b58af92a268a
dov11 <ovo.dan@gmail.com>
2017-11-11 15:12:58 +0100

    add basic index page with a list of artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
+  root to: "artists#index"
+  resources :artists do
+    resources :songs
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
