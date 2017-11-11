311f504f0e74019bb9b68b7044add9ee419db06e
dov11 <ovo.dan@gmail.com>
2017-11-12 00:30:32 +0100

    add create method and redirection to adding songs

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,12 +2,22 @@ class ArtistsController < ApplicationController
   def index
     @artists=Artist.all
   end
+
   def show
     @artist=Artist.find(params[:id])
   end
 
   def new
     @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to new_artist_song_path(@artist), notice: "Artist created, you can now add songs"
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -22,5 +32,9 @@ class ArtistsController < ApplicationController
 
   def set_artist
     @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :active_since)
   end
 end
