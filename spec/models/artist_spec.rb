bebd9a3aeb18db65612d2813bac4f7acf638e503
dov11 <ovo.dan@gmail.com>
2017-11-12 08:48:18 +0100

    add test for artist-song association

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Artist, type: :model do
       expect(artist.errors).to have_key(:name)
     end
   end
+  describe "association with song" do
+    let(:artist) { create :artist }
+    let!(:song) { create :song, artist: artist }
+
+    it "has many songs" do
+      song1 = artist.songs.new(name: "Wonderwall")
+      song2 = artist.songs.new(name: "Extraordinary")
+
+      expect(artist.songs).to include(song1)
+      expect(artist.songs).to include(song2)
+    end
+
+    it "deletes associated songs" do
+      expect { artist.destroy }.to change(Song, :count).by(-1)
+    end
+  end
 end
