269723ea949eab7ddffb15e2a78a32d00b24f1e6
dov11 <ovo.dan@gmail.com>
2017-11-18 15:03:04 +0100

    add testing of visiting newly created song

--- spec/features/manage_songs_spec.rb
+++ spec/features/manage_songs_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 feature 'Manage songs', js: true do
   let(:artist) { create :artist }
-  scenario 'add a new song' do
+  scenario 'add a new song and click on it' do
     visit artist_path(artist)
 
     fill_in "song_name", with: "Unique Song name"
 
     page.execute_script("$('#new_song').submit()")
+
+    expect(page).to have_content('Unique Song name')
+
+    sleep(1)
+
+    click_link("1")
+
+    sleep(1)
 
     expect(page).to have_content('Unique Song name')
   end
